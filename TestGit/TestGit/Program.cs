82ab437c9ad3f4962924a95a8c807c1986dea628
mahimanayak <mnayak@conserviscorp.com>
2015-07-20 10:41:36 -0500

    added unit test code

--- TestGit/TestGit/Program.cs
+++ TestGit/TestGit/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace TestGit
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Math _m=new Math();
+            Console.Write(_m.add(3, 4));
         }
     }
 }
