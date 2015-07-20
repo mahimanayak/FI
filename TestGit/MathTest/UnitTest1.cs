04ea9ab045e0ec2f5bb4bb23321fd09d671ad4eb
mahimanayak <mnayak@conserviscorp.com>
2015-07-20 15:38:22 -0500

    add missing files

---
+++ TestGit/MathTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestGit;
+
+namespace MathTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestGit.Math _m = new TestGit.Math();
+            int result=_m.add(4,3);
+            Assert.AreEqual(result, 7);
+        }
+    }
+}
---
+++ TestGit/TestGit/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    public class Math
+    {
+
+        public int add(int a,int b)
+        {            
+            return a + b;
+        }
+    }
+}
