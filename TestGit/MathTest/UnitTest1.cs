b9c475fdf60210fa55c0f8e2b33a3ae475840ec5
mahimanayak <mnayak@conserviscorp.com>
2015-08-06 10:38:00 -0500

    added new test

--- TestGit/MathTest/UnitTest1.cs
+++ TestGit/MathTest/UnitTest1.cs
@@ -8,14 +8,21 @@ namespace MathTest
     [TestClass]
     public class UnitTest1
     {        
-        [TestCase(1,2)]
+        [TestCase(5,2)]
         public void TestMethod1(int a,int b)
         {
             TestGit.Math _m = new TestGit.Math();
 
-            NUnit.Framework.Assert.AreEqual(_m.add(a, b), 3);
+            NUnit.Framework.Assert.AreEqual(_m.add(a, b), 7);
         }
 
+        [TestCase(8, 7)]
+        public void TestMethod2(int a, int b)
+        {
+            TestGit.Math _m = new TestGit.Math();
+
+            NUnit.Framework.Assert.AreNotEqual(_m.add(a, b), 7);
+        }
        
     }
 }
